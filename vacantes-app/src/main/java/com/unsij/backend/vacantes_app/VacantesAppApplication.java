8037571ed323926d4e7de4673de491fcb0a5d61c
EfrenDLD <24mdavid25@gmail.com>
2025-10-27 16:46:33 -0600

    Primer commit - estructura inicial del proyecto

---
+++ vacantes-app/src/main/java/com/unsij/backend/vacantes_app/VacantesAppApplication.java
@@ -0,0 +1,13 @@
+package backend.vacantes_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacantesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacantesAppApplication.class, args);
+	}
+
+}
---
+++ vacantes-app/src/test/java/backend/vacantes_app/VacantesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.vacantes_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacantesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
