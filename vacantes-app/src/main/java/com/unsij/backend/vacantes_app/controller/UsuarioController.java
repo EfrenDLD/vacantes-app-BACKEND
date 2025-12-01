3a0927a6eb90beb3397d236d2e098fd6766b5e39
Hernandez-Luis <luisalbertohdzrmz2003@gmail.com>
2025-12-01 15:27:07 -0600

    feat: Se agregaron servicios para eliminar

--- vacantes-app/src/main/java/com/unsij/backend/vacantes_app/controller/UsuarioController.java
+++ vacantes-app/src/main/java/com/unsij/backend/vacantes_app/controller/UsuarioController.java
@@ -1,6 +1,9 @@
 package com.unsij.backend.vacantes_app.controller;
 
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.unsij.backend.vacantes_app.model.Usuario;
@@ -20,5 +23,17 @@ public class UsuarioController {
     @GetMapping
     public List<Usuario> getAllUsuarios() {
         return usuarioService.getAll();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> delete(@RequestParam Long id) {
+        try {
+            usuarioService.deleteById(id);
+            return ResponseEntity.ok().body("Usuario eliminado correctamente");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error interno del servidor: DELETE");
+        }
     }
 }
